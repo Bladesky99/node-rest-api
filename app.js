dcc1dd14d602f62180fdcf54e79aee7c03ffc8ec
Bladesky99 <elisagonzalez@live.nl>
2023-10-12 16:55:34 +0200

    structure setup and install nodemon

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const { response } = require('express');
 const express = require('express');
 const { status } = require('express/lib/response');
+const MongoClient = require('mongodb').MongoClient;
 const port = 3000;
-const app = express ();
+const app = express ();  
 app.use(express.json());  
 
 app.listen(port, () => {
     console.log("Server Listening on PORT:", port);
   });
 
-  app.get("/status", (request, response) => {
-    const status = {
-        "Status": "Running"
-    };
+const characters = [
+  {id: 1, name: "John"},
+  {id: 2, name:"Jane"},
+  {id: 3, name:"David"},
+];
 
-    response.send(status);
-  })
+app.get("/status", (request, response) => {
+  const status = {
+      "Status": "Running"
+  };
+
+  response.send(status);
+})
+
+app.get("/characters", (req, res) => {
+  res.send(characters);
+})
+
+app.post("/characters", (req, res) => {
+  const character = {
+    id: characters.length + 1, 
+    name: req.body.name
+  };
+  characters.push(characters);
+  res.send(characters);
+})
+
+  
